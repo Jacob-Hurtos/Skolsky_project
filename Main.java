d2c14d966d589f008bb412f1875853c08482953b
Jacob-Hurtos <75687660+Jacob-Hurtos@users.noreply.github.com>
2022-02-01 07:13:19 +0100

    Vyhľadávacie_alg

--- Main.java
+++ Main.java
@@ -1,53 +1,67 @@
 package com.company;
-
-
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
+public class Main { int bin_search(int pole[], int first_index, int last_index, int cislo) {
+    if (first_index >= last_index) {
+        int mid = last_index + (first_index - last_index) / 2;
+        if (pole[mid] == cislo)
+            return mid;
+        if (pole[mid] > cislo)
+            return bin_search(pole, first_index, mid - 1, cislo);
+        return bin_search(pole, mid + 1, first_index, cislo);
+    }
 
+    return -1;
+}
 
-public class Main {
 
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int pole[] = new int[10];
-    for (int i = 0; i<pole.length;i++) {
-        pole[i] = 10 - i;
-    }
-        for(int prvok : pole){
+
+        Scanner sc = new Scanner(System.in);
+        Random rnd = new Random();
+
+        int n = rnd.nextInt(25); //nahodne velke pole do 25 prvkov
+        System.out.println("pole je o velkosti:" + n);
+        int pole[] = new int[n];
+        for (int i = 0; i < pole.length; i++) {
+            pole[i] = i + n;
+        }
+
+        Arrays.sort(pole);
+        for (int prvok : pole) {
             System.out.print(prvok + ",");
-        };
+        }
         System.out.println();
-        System.out.println("--------------------------------------------");
+        System.out.println("zadaj hladany prvok");
 
+        int cislo = sc.nextInt();
 
+        int first_index = pole[0];
+        int last_index = pole[n - 1];
+        int mid = first_index + (last_index - first_index) / 2;
+        System.out.println("Stred je:" + mid);
 
-
-    Random rnd = new Random();
-    int pole2[] = new int[20];
-    double priemer = 0;
-    int sucet = 0;
-
-
-    for (int j = 0; j<pole2.length;j++) {
-        int cislo= rnd.nextInt(1,50);
-        pole2[j]= cislo;
+        if (cislo == mid) {
+            System.out.println("prvok sa nasiel");
+            System.exit(1);
         }
-    for(int prvok :pole2  ){
-        System.out.print(prvok + ",");
-        }
-    for (int j = 0; j <pole2.length; j++) {
-        sucet=sucet+pole2[j];
-        }
-        priemer= sucet / pole2.length;
+        else if (cislo < mid) {
+            System.out.println(mid + " nie je cislo ktore hladam");
+            System.out.println(mid + " je > ako " + cislo);
 
-        Arrays.sort(pole2);
-        System.out.println();
-        System.out.println("sucet: "+sucet);
-        System.out.println("Priemer: "+priemer);
-        System.out.println("max cislo je: "+pole2[19]);
-        System.out.println("min cislo je: "+pole2[0]);
+        }
 
-    }}
+           else if (mid < cislo) {
+               System.out.println(mid + " nie je cislo ktore hladam");
+            System.out.println(mid + " je < ako " + cislo);
+        }
+
+    }
+}
+
+
+
+
 
